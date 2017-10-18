65a8c269148164558c6fa8532559f8f19c590057
Phil <philigjr@gmail.com>
2017-10-18 11:20:21 +0100

    Created fire alias
    Created Alias for the fire method

--- src/commands/MigrationCommand.php
+++ src/commands/MigrationCommand.php
@@ -6,21 +6,18 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
 class MigrationCommand extends Command {
-
     /**
      * The console command name.
      *
      * @var string
      */
     protected $name = 'cities:migration';
-
     /**
      * The console command description.
      *
      * @var string
      */
     protected $description = 'Creates a migration following the Laravel-cities specifications.';
-
     /**
      * Create a new command instance.
      *
@@ -32,45 +29,47 @@ class MigrationCommand extends Command {
         $app = app();
         $app['view']->addNamespace('cities',substr(__DIR__,0,-8).'views');
     }
-
     /**
      * Execute the console command.
      *
      * @return void
      */
-    public function handle()
+    public function fire()
     {
         $this->line('');
         $this->info('The migration process will create a migration file and a seeder for the cities list');
-
         $this->line('');
-
         if ( $this->confirm("Proceed with the migration creation? [Yes|no]") )
         {
             $this->line('');
-
             $this->info( "Creating migration and seeder..." );
             if( $this->createMigration( 'cities' ) )
             {
                 $this->line('');
-
-                $this->call('optimize', array());
-
+                if (version_compare(app()->version(), '5.5.0', '<')) {
+                    $this->call('optimize', []);
+                }
                 $this->line('');
-
                 $this->info( "Migration successfully created!" );
             }
             else{
-                $this->error(
+                $this->error( 
                     "Coudn't create migration.\n Check the write permissions".
                     " within the app/database/migrations directory."
                 );
             }
-
             $this->line('');
         }
     }
-
+    /**
+     * Alias fire method.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->fire();
+    }
     /**
      * Get the console command options.
      *
@@ -78,9 +77,8 @@ class MigrationCommand extends Command {
      */
     protected function getOptions()
     {
-        return array();
+        return [];
     }
-
     /**
      * Create the migration
      *
@@ -91,36 +89,28 @@ class MigrationCommand extends Command {
     {
         //Create the migration
         $app = app();
-        $migrationFiles = array(
+        $migrationFiles = [
             $this->laravel->path."/../database/migrations/*_setup_cities_table.php" => 'cities::generators.migration',
-            $this->laravel->path."/../database/migrations/*_charify_cities_table.php" => 'cities::generators.char_migration'
-        );
-
+            $this->laravel->path."/../database/migrations/*_charify_cities_table.php" => 'cities::generators.char_migration',
+        ];
         $seconds = 0;
-
-        foreach ($migrationFiles as $migrationFile => $outputFile) {
+        foreach ($migrationFiles as $migrationFile => $outputFile) {            
             if (sizeof(glob($migrationFile)) == 0) {
                 $migrationFile = str_replace('*', date('Y_m_d_His', strtotime('+' . $seconds . ' seconds')), $migrationFile);
-
                 $fs = fopen($migrationFile, 'x');
                 if ($fs) {
                     $output = "<?php\n\n" .$app['view']->make($outputFile)->with('table', 'cities')->render();
-
                     fwrite($fs, $output);
                     fclose($fs);
                 } else {
                     return false;
                 }
-
                 $seconds++;
             }
         }
-
-
         //Create the seeder
-        $seeder_file = $this->laravel->path."/../database/seeds/CitiesSeeder.php";
+        $seeder_file = $this->laravel->path."/../database/seeds/citiesSeeder.php";
         $output = "<?php\n\n" .$app['view']->make('cities::generators.seeder')->render();
-
         if (!file_exists( $seeder_file )) {
             $fs = fopen($seeder_file, 'x');
             if ($fs) {
@@ -130,8 +120,6 @@ class MigrationCommand extends Command {
                 return false;
             }
         }
-
         return true;
     }
-
 }
